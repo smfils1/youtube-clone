4b383db1135fd282adf42b0f7294d3fb3c249b64
smfils1 <smfils1@gmail.com>
2020-07-11 08:37:09 -0400

    Fix routes for video saving & thumbnail generation

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -9,39 +9,125 @@ const auth = require("../middleware/auth");
 const video = require("../utils/video");
 const Video = require("../models/video");
 
+const fs = require("fs");
+const util = require("util");
+const path = require("path");
+const readFile = util.promisify(fs.readFile);
+
 const config = require("../config");
 
+// Save video
+// router.post(
+//   "/",
+//   /*auth, */ (req, res) => {
+//     const {
+//       videoPath: filePath,
+//       thumbnails,
+//       title,
+//       description,
+//       privacy,
+//       category,
+//     } = req.body;
+//     const selectedThumbPath = thumbnails.selected;
+//     const nonSelectedThumbPaths = thumbnails.nonSelected;
+//     const { _id: uploader } = req.user;
+
+//     try {
+//       video.upload(req, res, (err) => {
+//         if (err) {
+//           res.status(400).json({
+//             name: "UploadError",
+//             message: err.message,
+//           });
+//         } else {
+//           res.json({
+//             success: true,
+//             filePath: res.req.file.path,
+//             fileName: res.req.file.filename,
+//           });
+//         }
+//       });
+//     } catch (err) {
+//       res.status(500).json({
+//         name: "ServerError",
+//         message: err.message,
+//       });
+//     }
+//   }
+// );
+
+// Save video
 router.post(
   "/",
   /*auth, */ (req, res) => {
-    //TODO: Error Handling
-    video.upload(req, res, (err) => {
-      if (err) {
-        return res.json({ success: false, err });
-      }
-      return res.json({
-        success: true,
-        filePath: res.req.file.path,
-        fileName: res.req.file.filename,
+    try {
+      video.upload(req, res, (err) => {
+        if (err) {
+          res.status(400).json({
+            name: "UploadError",
+            message: err.message,
+          });
+        } else {
+          res.json({
+            success: true,
+            filePath: res.req.file.path,
+            fileName: res.req.file.filename,
+          });
+        }
       });
-    });
+    } catch (err) {
+      res.status(500).json({
+        name: "ServerError",
+        message: err.message,
+      });
+    }
   }
 );
 
+//Generate thumbs
 router.post(
-  "/thumbnail",
-  /*auth, */ (req, res) => {
-    //TODO: Error Handling
-    video.upload(req, res, (err) => {
-      if (err) {
-        return res.json({ success: false, err });
-      }
-      return res.json({
-        success: true,
-        filePath: res.req.file.path,
-        fileName: res.req.file.filename,
+  "/thumbnails",
+  /*auth, */ async (req, res) => {
+    const { filename } = req.body;
+    console.log(filename);
+    try {
+      const thumbLinks = await video.generateThumbnails(
+        path.join("data", "videos", filename),
+        3
+      );
+
+      res.json({ thumbnails: thumbLinks });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        name: "ServerError",
+        message: err.message,
       });
-    });
+    }
+  }
+);
+
+//Get thumb
+router.get(
+  "/thumbnail/:thumbFile",
+  /*auth, */ async (req, res) => {
+    const { thumbFile } = req.params;
+    try {
+      const file = path.join(__dirname, "..", video.localThumbPath, thumbFile);
+      res.sendFile(file, function (err) {
+        if (err) {
+          res.status(err.status).json({
+            name: "FileError",
+            message: "File do not exist",
+          });
+        }
+      });
+    } catch (err) {
+      res.status(500).json({
+        name: "ServerError",
+        message: err.message,
+      });
+    }
   }
 );
 

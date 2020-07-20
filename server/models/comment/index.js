ed56e258fff2a69decc8f7adab097609fc81b432
smfils1 <smfils1@gmail.com>
2020-07-20 09:24:08 -0400

    Added initial comment model for db

---
+++ server/models/comment/index.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const commentSchema = mongoose.Schema(
+  {
+    commentBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "{PATH} is required"],
+    },
+    videoId: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+      required: [true, "{PATH} is required"],
+    },
+    content: {
+      type: String,
+      required: [true, "{PATH} is required"],
+    },
+    commentTo: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+require("./methods")(commentSchema);
+const Comment = mongoose.model("Comment", commentSchema);
+
+module.exports = Comment;
---
+++ server/models/comment/methods.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose");
+
+const methods = (commentSchema) => {};
+
+module.exports = methods;

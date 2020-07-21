2ab05f2dfd550f051f76519280897de5ae354d6d
smfils1 <smfils1@gmail.com>
2020-07-21 14:07:17 -0400

    Added video votes db methods

--- server/models/videoVote/methods.js
+++ server/models/videoVote/methods.js
@@ -1,5 +1,49 @@
-const mongoose = require("mongoose");
+const methods = (videoVoteSchema) => {
+  videoVoteSchema.statics.getVotes = async function (videoId) {
+    const VideoVote = this;
+    let results;
+    const filter = {
+      videoId,
+    };
+    try {
+      results = await VideoVote.aggregate([
+        { $match: filter },
+        {
+          $group: {
+            _id: "$vote",
+            count: { $sum: 1 },
+          },
+        },
+      ]);
+      let votes;
+      votes.forEach((voteGroup) => {
+        vote[voteGroup._id] = voteGroup.count;
+      });
+      return votes;
+    } catch (err) {
+      throw err;
+    }
+  };
 
-const methods = (videoVoteSchema) => {};
+  videoVoteSchema.statics.updateVoteByUserId = async function ({
+    videoId,
+    userId,
+    vote,
+  }) {
+    const VideoVote = this;
+    const filter = {
+      videoId,
+      userId,
+    };
+    const update = { vote };
+    try {
+      const vote = await VideoVote.findOneAndUpdate(filter, update);
+      if (!vote) vote = null;
+      return vote;
+    } catch (err) {
+      throw err;
+    }
+  };
+};
 
 module.exports = methods;
